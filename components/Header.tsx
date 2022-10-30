505ea4352a97efbb4efc1346f25c6811e86d205d
Mujeeb Adebowale <olukayodedayo100@gmail.com>
2022-10-30 15:41:00 +0100

    header-completed

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,19 +3,27 @@ import React from 'react'
 
 function Header() {
   return (
-      <header>
-          <div>
+      <header className='flex justify-between p-5 mx-auto max-w-7xl'>
+          <div className='flex items-center space-x-5'>
               <Link href="/">
                   <img className="object-contain cursor-pointer w-44"
                       src="https://links.papareact.com/yvf" alt="" />
               </Link>
-              <div>
-                  <h3></h3>
-                  <h3></h3>
-                  <h3></h3>
+              <div className='items-center hidden space-x-5 md:inline-flex'>
+                  <h3>About</h3>
+                  <h3>Contact</h3>
+                  <h3 className='px-4 py-1 text-white bg-green-600 rounded-full'>Follow</h3>
               </div>
           </div>
-          <div></div>
+          <div className='flex items-center space-x-5 text-green-600'>
+              <h3>
+                  Sign in
+              </h3>
+              <h3 className='px-4 py-1 border border-green-600 rounded-full'>
+                  Get Started
+                  
+              </h3>
+          </div>
     </header>
   )
 }
